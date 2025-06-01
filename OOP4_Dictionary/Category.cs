97ca6de303adc6bbd61605d90d6ec298e1a03d86
Salmon <90376520+SalmonDXH@users.noreply.github.com>
2025-06-01 17:36:37 +0700

    upload

---
+++ OOP4_Dictionary/Category.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4_Dictionary
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Dictionary<int, Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new Dictionary<int, Product>();
+        }
+
+        // CRUD
+        public void AddProduct(Product p)
+        {
+            if (Products.ContainsKey(p.Id) == false)
+            {
+                Products.Add(p.Id, p);
+            }
+        }
+
+        public void PrintAllProducts()
+        {
+            foreach(KeyValuePair<int, Product> item in Products)
+            {
+                Product p = item.Value;
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+        public Product GetProduct(int id)
+        {
+            if (Products.ContainsKey(id) == false){
+                return null;
+            }
+            return Products[id];
+        }
+
+    }
+}
---
+++ OOP4_Dictionary/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using OOP4_Dictionary;
+
+Category Laptop = new Category();
+Laptop.Id = 1;
+Laptop.Name = "Danh Mục Laptop";
+
+Product dell = new Product()
+{
+    Id = 1,
+    Name = "Dell",
+    Quantity = 100,
+    Price = 100000
+};
+
+Product dell2 = new Product()
+{
+    Id = 2,
+    Name = "Dell2",
+    Quantity = 1,
+    Price = 2000
+};
+
+Product hp = new Product()
+{
+    Id = 3,
+    Name = "hp",
+    Quantity = 1,
+    Price = 40230
+};
+
+Laptop.AddProduct(dell);
+Laptop.AddProduct(dell2);
+Laptop.AddProduct(hp);
+
+Console.WriteLine("Danh sach san pham");
+Laptop.PrintAllProducts();
+
+Console.WriteLine("Nhap ma san pham:");
+int pid = int.Parse(Console.ReadLine());
+Product p = Laptop.GetProduct(pid);
+if (p == null){
+    Console.WriteLine("Ko tim thay san pham");
+} else
+{
+    Console.WriteLine("Tim thay san pham:");
+    Console.WriteLine(p.ToString());
+}
+
+
+
+Product newp = new Product();
+
+newp.Id = 4;
+
+Console.ReadLine(); 
+
